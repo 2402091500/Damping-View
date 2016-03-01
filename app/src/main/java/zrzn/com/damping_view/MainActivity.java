55c65cd6b4dfbf3e46e43615fd729f6dada8dcef
ZRZn <547719849@qq.com>
2016-03-01 22:22:50 +0800

    first push

---
+++ app/src/main/java/zrzn/com/damping_view/MainActivity.java
@@ -0,0 +1,13 @@
+package zrzn.com.damping_view;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
